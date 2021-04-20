200eb58e9c77c744118e1af2181c62483429f74f
dijkwxyz <465221648@qq.com>
2021-04-20 21:02:41 +0800

    Update AdvertisingTopologyFlinkWindows.java

--- flink-benchmarks/src/main/java/flink/benchmark/AdvertisingTopologyFlinkWindows.java
+++ flink-benchmarks/src/main/java/flink/benchmark/AdvertisingTopologyFlinkWindows.java
@@ -8,7 +8,6 @@ import benchmark.common.advertising.RedisAdCampaignCache;
 import com.dijk.multilevel.PatternBasedMultilevelStateBackend;
 import flink.benchmark.generator.EventGeneratorSource;
 import flink.benchmark.generator.RedisHelper;
-import flink.benchmark.utils.StateBackendFactory;
 import flink.benchmark.utils.ThroughputLogger;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
