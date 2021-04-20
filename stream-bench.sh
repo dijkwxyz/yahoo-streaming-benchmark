4751986d90dfeef1e020dbb23fb9a889d13a3c6f
dijkwxyz <465221648@qq.com>
2021-04-20 21:21:34 +0800

    conf path

--- stream-bench.sh
+++ stream-bench.sh
@@ -266,7 +266,7 @@ run() {
     stop_if_needed KafkaDataGenerator "Load Generation"
 #    stop_if_needed leiningen.core.main "Load Generation"
     cd data
-    java -cp /home/ec2-user/yahoo-streaming-benchmark/flink-benchmarks/target/flink-benchmarks-0.1.0.jar flink.benchmark.utils.RedisDataGetter $CONF_FILE
+    java -cp /home/ec2-user/yahoo-streaming-benchmark/flink-benchmarks/target/flink-benchmarks-0.1.0.jar flink.benchmark.utils.RedisDataGetter $BASE_DIR/$CONF_FILE
 #    $LEIN run -g --configPath ../$CONF_FILE || true
     cd ..
     scp $BASE_DIR/data/seen-updated-subtask.txt ec2-user@ZK_HOST:$BASE_DIR/data/seen-updated-subtask.txt
