9aafb17a20b039cb6dc78a4df2e5efefcc793e81
Dijkwxyz <465221648@qq.com>
2021-07-03 15:13:55 +0800

    print experiment.sh param in conf comment

--- experiment.sh
+++ experiment.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-CHECKPOINT_INTERVAL_MS=${CHECKPOINT_INTERVAL_MS:-60000}
-MULTILEVEL_ENABLE=${MULTILEVEL_ENABLE:-false}
-LOAD=${LOAD:-100000}
+# used for stream-bench.sh
 TEST_TIME=${TEST_TIME:-600}
-MTTI_MS=${MTTI_MS:--1}
-FAILURE_INTERVAL=${FAILURE_INTERVAL:--1}
+TM_FAILURE_INTERVAL=${TM_FAILURE_INTERVAL:--1}
 
-BASE_DIR=${BASE_DIR:-/home/ec2-user/yahoo-streaming-benchmark/}}
-CONF_FILE=${CONF_FILE:-conf/benchmarkConf.yaml}
+# used for conf/benchmarkConf.yaml
+CHECKPOINT_INTERVAL_MS=${CHECKPOINT_INTERVAL_MS:-60000}
+MTTI_MS=${MTTI_MS:--1}
+MULTILEVEL_ENABLE=${MULTILEVEL_ENABLE:-false}
+
 WINDOW_SIZE=${WINDOW_SIZE:-60}
 WINDOW_SLIDE=${WINDOW_SLIDE:-1}
+
+LOAD=${LOAD:-100000}
 NUM_CAMPAIGNS=${NUM_CAMPAIGNS:-100}
 USE_LOCAL_GENERATOR=${USE_LOCAL_GENERATOR:-false}
 REDIS_FLUSH=${REDIS_FLUSH:-false}
+
+# other
+BASE_DIR=${BASE_DIR:-/home/ec2-user/yahoo-streaming-benchmark/}}
+CONF_FILE=${CONF_FILE:-conf/benchmarkConf.yaml}
+
 
 make_conf() {
     echo "# Copyright 2015, Yahoo Inc.
 # Licensed under the terms of the Apache License 2.0. Please see LICENSE file in the project root for terms.
 
+# ====== experiment.sh config ======
+# TEST_TIME=$TEST_TIME
+# TM_FAILURE_INTERVAL=$TM_FAILURE_INTERVAL
+#
 # ============ kafka ============
 kafka.brokers:
     - \"kafka1\"
@@ -80,5 +91,5 @@ for (( LOAD=800000; LOAD <= 1000000; LOAD += 100000 )); do
   echo "start experiment with LOAD = $LOAD, TIME = $TEST_TIME"
   make_conf
   xsync $CONF_FILE
-  ./stream-bench.sh $TEST_TIME $FAILURE_INTERVAL CLUSTER_TEST
+  ./stream-bench.sh $TEST_TIME $TM_FAILURE_INTERVAL CLUSTER_TEST
 done
