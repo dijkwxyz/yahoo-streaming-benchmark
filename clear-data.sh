75ecad21028972807c0421125bce67d30af22245
dijkwxyz <465221648@qq.com>
2021-09-10 10:27:43 +0000

    clear data

--- clear-data.sh
+++ clear-data.sh
@@ -4,6 +4,8 @@ BASE_DIR=`pwd`
 hadoop fs -rm -r /flink
 
 xdo "rm -r /tmp/flink*"
+xdo "rm -r /tmp/localState"
+xdo "rm -r /tmp/blobStore-*"
 xdo "rm -r /tmp/kafka-logs/"
 xdo "rm $BASE_DIR/results/*.*"
 xdo "cd $BASE_DIR; ./stream-bench.sh CLEAR_LOGS"
