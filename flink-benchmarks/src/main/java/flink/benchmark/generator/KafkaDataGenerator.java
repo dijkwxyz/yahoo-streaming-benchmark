fe2b5c8dc80707f277348555e9e2f75f33542596
dijkwxyz <465221648@qq.com>
2021-09-22 16:20:56 +0800

    generate random events type instead of one by one

--- flink-benchmarks/src/main/java/flink/benchmark/generator/KafkaDataGenerator.java
+++ flink-benchmarks/src/main/java/flink/benchmark/generator/KafkaDataGenerator.java
@@ -105,7 +105,7 @@ public class KafkaDataGenerator {
         sb.append("\",\"ad_type\":\"");
         sb.append("banner78"); // value is immediately discarded. The original generator would put a string with 38/5 = 7.6 chars. We put 8.
         sb.append("\",\"event_type\":\"");
-        sb.append(eventTypes[eventsIdx++]);
+        sb.append(eventTypes[new Random().nextInt(eventTypes.length)]);
         sb.append("\",\"event_time\":\"");
         sb.append(System.currentTimeMillis());
         sb.append("\",\"ip_address\":\"1.2.3.4\"}");
