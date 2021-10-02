c6014ee6d1728bca8c37827cd9d65666007ef66c
dijkwxyz <465221648@qq.com>
2021-10-02 11:09:58 +0800

    failure injector bug

--- flink-benchmarks/src/main/java/flink/benchmark/utils/FailureInjectorMap.java
+++ flink-benchmarks/src/main/java/flink/benchmark/utils/FailureInjectorMap.java
@@ -2,27 +2,16 @@ package flink.benchmark.utils;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.flink.api.common.functions.RichMapFunction;
-import org.apache.flink.api.common.state.ListState;
-import org.apache.flink.api.common.state.ListStateDescriptor;
-import org.apache.flink.api.common.state.ValueState;
-import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.runtime.state.FunctionInitializationContext;
-import org.apache.flink.runtime.state.FunctionSnapshotContext;
-import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
-import org.apache.flink.streaming.api.functions.ProcessFunction;
-import org.apache.flink.util.Collector;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
     /**
      * failure rate at this operator
      */
-    private double localFailureRatePerMs;
+    private double localFailureRatePerSec;
     /**
      * MTTI in milliseconds for the whole application
      */
@@ -38,6 +27,7 @@ public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
     private long startTimeDelayMs;
     private long startTimeMs;
 //    private ListState<Long> startTimeMsState;
+
     /**
      * @param globalMttiMs mean time to interrupt in milliseconds
      */
@@ -45,7 +35,7 @@ public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
         this.parallelism = parallelism;
         this.globalMttiMilliSeconds = globalMttiMs;
         this.injectWithProbability = injectWithProbability;
-        this.localFailureRatePerMs = 1.0 / globalMttiMs / parallelism;
+        this.localFailureRatePerSec = 1000.0 / globalMttiMs / parallelism;
         this.injectFailures = globalMttiMs > 0;
         this.startTimeDelayMs = startTimeDelayMs;
         this.startTimeMs = startTimeDelayMs + System.currentTimeMillis();
@@ -53,27 +43,40 @@ public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
     }
 
     /**
-     *
      * @param args
      */
     public static void main(String[] args) throws Exception {
         long prev = System.currentTimeMillis();
         System.out.println(prev);
-        FailureInjectorMap failureInjectorMap = new FailureInjectorMap(1000, false,1, 0L);
+        FailureInjectorMap failureInjectorMap = new FailureInjectorMap<Integer>(10000, true, 1, 0L);
         DescriptiveStatistics ds = new DescriptiveStatistics();
-        while (true) {
-            failureInjectorMap.map(1);
-        }
-        //get 100 failures and calculate MTTI
-//        while (ds.getN() < 100) {
-//            if (failureInjectorMap.test()) {
-//                long curr = System.currentTimeMillis();
-//                ds.addValue(curr - prev);
-//                prev = curr;
-//            }
+//        while (true) {
+//            failureInjectorMap.map(1);
 //        }
-//        System.out.println(ds.getMean());
+        // get 100 failures and calculate MTTI
+        int size = 10;
+        long[] res = new long[size];
+        int ct = 0;
+        while (ct < size) {
+            try {
+                failureInjectorMap.map(1);
+            } catch (RuntimeException e) {
+                long curr = System.currentTimeMillis();
+                res[ct] = curr - prev;
+                prev = curr;
+                ct++;
+                failureInjectorMap = new FailureInjectorMap<Integer>(10000, true, 1, 0L);
+            }
+        }
+
+        long sum = 0;
+        for (long l : res) {
+            sum += l;
+        }
+
+        System.out.println((double) sum / res.length);
     }
+
     /**
      * inject failure based on failure rate
      */
@@ -82,12 +85,11 @@ public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
         if (prevTime < 0) {
             prevTime = currTime;
         }
-        if (currTime - prevTime > 0) {
+        if (currTime - prevTime > 100) {
             double roll = new Random().nextDouble();
-            if (roll < (currTime - prevTime) * getLocalFailureRatePerMs()) {
+            if (roll < (double) ((currTime - prevTime) / 1000) * getLocalFailureRatePerSec()) {
                 startTimeMs = startTimeDelayMs + System.currentTimeMillis();
                 injectFailure(currTime);
-                return;
             }
             prevTime = currTime;
         }
@@ -103,7 +105,7 @@ public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
         long timeDiff = currTime - prevTime;
         if (timeDiff > 0) {
             double roll = new Random().nextDouble();
-            if (roll < timeDiff * getLocalFailureRatePerMs()) {
+            if (roll < timeDiff * getLocalFailureRatePerSec()) {
                 return true;
             }
             prevTime = currTime;
@@ -111,12 +113,12 @@ public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
         return false;
     }
 
-    public double getLocalFailureRatePerMs() {
-        return localFailureRatePerMs;
+    public double getLocalFailureRatePerSec() {
+        return localFailureRatePerSec;
     }
 
     public void setFailureRate(double failureRatePerMs) {
-        this.localFailureRatePerMs = failureRatePerMs;
+        this.localFailureRatePerSec = failureRatePerMs;
     }
 
     @Override
@@ -129,8 +131,7 @@ public class FailureInjectorMap<T> extends RichMapFunction<T, T> {
             if (System.currentTimeMillis() > startTimeMs) {
                 maybeInjectFailure();
             }
-        }
-        else {
+        } else {
             long currentTimeMillis = System.currentTimeMillis();
             if (currentTimeMillis > startTimeMs + globalMttiMilliSeconds) {
                 injectFailure(currentTimeMillis);
